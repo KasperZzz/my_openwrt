ed05340ced22dff03e7bf0cf36fdee4b74aa8712
KasperZzz <106367560+KasperZzz@users.noreply.github.com>
2026-04-16 17:51:19 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Добавляем репозиторий Passwall
+echo 'src-git passwall_packages https://github.com' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com' >> feeds.conf.default
+
+# Добавляем репозиторий Zapret
+echo 'src-git zapret https://github.com' >> feeds.conf.default
