69b5643879a38abf54840a9eb6a2965455749f2f
KasperZzz <106367560+KasperZzz@users.noreply.github.com>
2026-04-16 18:20:58 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,23 +2,12 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Удаляем встроенный helloworld (если он есть), чтобы избежать конфликтов
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# Добавляем репозиторий Passwall
+# Добавляем правильные и полные ссылки на репозитории
 echo 'src-git passwall_packages https://github.com' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com' >>feeds.conf.default
-
-# Добавляем репозиторий Zapret
 echo 'src-git zapret https://github.com' >>feeds.conf.default
