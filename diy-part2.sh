21973d9ca2a1131f659d4704e610d4961a47393a
KasperZzz <106367560+KasperZzz@users.noreply.github.com>
2026-04-16 18:21:32 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,34 +1,9 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# Принудительно отключаем Xray, так как Sing-box более универсален и экономит место
+echo "CONFIG_PACKAGE_xray-core=n" >> .config
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Отключаем встроенный в LEDE софт, который дублирует функции и занимает место
+echo "CONFIG_PACKAGE_luci-app-ssr-plus=n" >> .config
+echo "CONFIG_PACKAGE_luci-app-vssr=n" >> .config
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-# Настройка модели
-echo "CONFIG_TARGET_mediatek=y" >>.config
-echo "CONFIG_TARGET_mediatek_filogic=y" >>.config
-echo "CONFIG_TARGET_mediatek_filogic_DEVICE_cudy_wr3000-v1=y" >>.config
-
-# Пакеты
-echo "CONFIG_PACKAGE_luci-app-passwall=y" >>.config
-echo "CONFIG_PACKAGE_sing-box=y" >>.config
-echo "CONFIG_PACKAGE_zapret=y" >>.config
-echo "CONFIG_PACKAGE_luci-app-zapret=y" >>.config
-
-# Удаляем лишнее для экономии места
-echo "CONFIG_PACKAGE_wpad-openssl=n" >>.config
-echo "CONFIG_PACKAGE_wpad-basic-mbedtls=y" >>.config
+# Добавляем поддержку русского языка для интерфейса (по желанию)
+echo "CONFIG_LUCI_LANG_ru=y" >> .config
